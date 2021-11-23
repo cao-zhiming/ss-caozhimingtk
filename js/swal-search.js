522e37b244c717a99f9910cdd7adb637be8fede0
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2021-11-23 18:55:31 +0800

    Update swal-search.js

--- js/swal-search.js
+++ js/swal-search.js
@@ -1,25 +1,27 @@
 import sweetalert.js
 import sweetalert
 
-swal({
-  text: '输入搜索内容……',
-  content: "input",
-  button: {
-    text: "输入……",
-    closeModal: false,
-  },
-})
-.then(name => {
-  if (!name) throw null;
+function swalsearch(){
+  swal({
+    text: '输入搜索内容……',
+    content: "input",
+    button: {
+      text: "输入……",
+      closeModal: false,
+    },
+  })
+  .then(name => {
+    if (!name) throw null;
  
-  window.location.href='https://czhiming.cn/?s=${name}';
-})
+    window.location.href='https://czhiming.cn/?s=${name}';
+  })
 
-.catch(err => {
-  if (err) {
-    swal("哦不", "请求错误", "error");
-  } else {
-    swal.stopLoading();
-    swal.close();
-  }
-});
+  .catch(err => {
+    if (err) {
+      swal("哦不", "请求错误", "error");
+    } else {
+      swal.stopLoading();
+      swal.close();
+    }
+  });
+}
